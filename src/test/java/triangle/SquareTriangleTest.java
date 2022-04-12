a06320de0f61b278bfb4ea5c95791cb69dfd80b9
Lidiya Makarova <li-ma@mail.ru>
2022-04-12 23:54:25 +0300

    commit commit

--- src/test/java/triangle/SquareTriangleTest.java
+++ src/test/java/triangle/SquareTriangleTest.java
@@ -2,23 +2,25 @@ package triangle;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.function.Executable;
 import static org.junit.Assert.assertEquals;
 
 public class SquareTriangleTest {
+
     @Test
     public void checkAnswer() {
         Assertions.assertAll("Should return answer",
-                () -> assertEquals(SquareTriangle.squareTriangle(3, 4, 5), "6.0"),
-                () -> assertEquals(SquareTriangle.squareTriangle(0, 0, 0), "Calculation with such data is not possible"),
-                () -> assertEquals(SquareTriangle.squareTriangle(0, 4, 5), "Calculation with such data is not possible"),
-                () -> assertEquals(SquareTriangle.squareTriangle(3, 0, 5), "Calculation with such data is not possible"),
-                () -> assertEquals(SquareTriangle.squareTriangle(3, 4, 0), "Calculation with such data is not possible"),
-                () -> assertEquals(SquareTriangle.squareTriangle(-3, -4, -5), "Calculation with such data is not possible"),
-                () -> assertEquals(SquareTriangle.squareTriangle(-3, 4, 5), "Calculation with such data is not possible"),
-                () -> assertEquals(SquareTriangle.squareTriangle(3, -4, 5), "Calculation with such data is not possible"),
-                () -> assertEquals(SquareTriangle.squareTriangle(1, 1, 3), "It's not a triangle"),
-                () -> assertEquals(SquareTriangle.squareTriangle(1, 3, 1), "It's not a triangle"),
-                () -> assertEquals(SquareTriangle.squareTriangle(3, 1, 1), "It's not a triangle")
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(3, 4, 5), "6.0"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(0, 0, 0), "Calculation with such data is not possible"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(0, 4, 5), "Calculation with such data is not possible"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(3, 0, 5), "Calculation with such data is not possible"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(3, 4, 0), "Calculation with such data is not possible"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(-3, -4, -5), "Calculation with such data is not possible"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(-3, 4, 5), "Calculation with such data is not possible"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(3, -4, 5), "Calculation with such data is not possible"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(1, 1, 3), "It's not a triangle"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(1, 3, 1), "It's not a triangle"),
+                (Executable) () -> assertEquals(SquareTriangle.squareTriangle(3, 1, 1), "It's not a triangle")
         );
     }
 }
